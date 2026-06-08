a3c9735a704b3e6ac1e63dc8477047d8a499c437
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-08 18:32:45 +0530

    Added printf and scanf program

---
+++ printffunction.c
@@ -0,0 +1,10 @@
+#include<stdio.h> // Standard Input Output 
+ 
+int main()  //Program Starts From main function
+ 
+ {
+    
+    printf("Hello Yash"); // Prints text on screen
+
+    return 0; // Program ends successfully
+}
---
+++ scanf.c
@@ -0,0 +1,15 @@
+#include<stdio.h> //Standard Input Output
+
+int main()  // Program Start From main Function
+
+
+{
+    int age  ; //variable to store age 
+
+    printf("Enter Age: "); //Ask user for input
+    scanf("%d", &age);      // Takes input from
+
+    printf("Age = %d", age); // Prints entered age
+
+    return 0;  // program ends successfully
+}
