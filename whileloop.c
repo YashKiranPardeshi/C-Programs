f7ca7f14548f553f2741ea41cb0d195f4db6f7d4
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-12 23:26:20 +0530

    Add While Loop Program in C language
    Demonstrates the use of a while loop in C to print numbers from 1 to 5. Useful for understanding loop execution and iteration.

---
+++ whileloop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+    int main()
+    {
+        int i = 1;//Initialize variable i with value 1 
+
+        while(i <= 5)//Loop will run until i become greater than
+        {
+                printf("%d\n", i); //print the value of i
+                i++; // Increment i by 1
+        }
+        return 0; // End of program
+    }
