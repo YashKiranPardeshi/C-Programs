44c59604f8b34e7e4f44e3ebf2dbd4e6e8b2206c
YashKiranPardeshi <100748729+YashKiranPardeshi@users.noreply.github.com>
2026-06-13 13:07:21 +0530

    Add Do-While Loop Program In C Language
    This program demonstrates the use of a do-while loop in C. It prints numbers from 1 to 5 and show that the loop executes at least once before checking the condition.

---
+++ dowhileloop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i = 1; //Declare and initialize variable i with value 1.
+
+    do // Start of do-while loop.
+    {
+        printf("%d\n", i ); // Print the current value of i.
+        i++; // increase the value of i by 1.
+    }
+    while(i <= 5); // Check condition after executing the loop body.
+                    // Loop continues until i becomes greater than 5
+    return 0 ;
+}
+
+
+
+
+
